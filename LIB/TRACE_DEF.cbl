8a3bc5182405c7a9a4ce49a991eaf3584a4acad1
Nigromante <nigromante@github.com>
2025-11-11 19:39:01 -0300

    Format text

--- LIB/TRACE_DEF.cbl
+++ LIB/TRACE_DEF.cbl
@@ -1,7 +1,8 @@
 
-           77 W001-Buffer     PIC X(2000).
-           77 W001-Parts  redefines W001-Buffer OCCURS 2000 TIMES PIC X. 
-           77 W001-Buffer-Len PIC 9999 VALUE 0.
-           77 W001-Idx PIC 9999 VALUE 1.
-           77 W001-Title     PIC A(10).
+           77 W001-Buffer      PIC X(2000).
+           77 W001-Parts  
+                 REDEFINES W001-Buffer OCCURS 2000 TIMES PIC X. 
+           77 W001-Buffer-Len  PIC 9999 VALUE 0.
+           77 W001-Idx         PIC 9999 VALUE 1.
+           77 W001-Title       PIC A(10).
 
