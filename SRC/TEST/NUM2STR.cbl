05bf1ea4dad38d3a8a9e8739e231caf71bc0a994
Nigromante <nigromante@github.com>
2025-11-14 12:01:27 -0300

    test lib num2str

---
+++ SRC/TEST/NUM2STR.cbl
@@ -0,0 +1,32 @@
+       identification division.
+       program-id.    NUM2STR.
+       author.        Julian Vidal.
+
+       environment division.
+       configuration section.
+       repository.
+           program LIBNUM2STR.
+
+       special-names.
+            console is crt
+            decimal-point is comma.
+
+       data division.
+
+       working-storage section.
+
+       01  parametro   PIC x(12).
+       01  texto       PIC x(2000).
+
+       procedure division.
+
+
+       main section.
+
+           MOVE "012345678901"  TO parametro.
+           display parametro line 10 position 10.
+           call LIBNUM2STR USING parametro texto.
+           display texto line 12 position 10.
+
+           stop run.
+
