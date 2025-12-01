a6f893c8be61746f7e5806ec6349531d20d440f1
Nigromante <nigromante@github.com>
2025-12-01 08:52:45 -0300

    crean apps

--- rrun.sh
+++ rrun.sh
@@ -2,6 +2,7 @@
 
 export COB_LIBRARY_PATH=~/Work/proyectos/cobol/BUILD/RELEASE/LIB
 
-make -f Makefile input=LIBNUM2STR module
-make -f Makefile input=NUM2STR compile
-make -f Makefile input=NUM2STR run
+make -f Makefile input=LIBNUM2ESSTR module
+make -f Makefile input=NUM2ESSTR compile
+make -f Makefile input=NUM2ESSTR run
+
