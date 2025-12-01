0ef38ada2d1759ee39d0a2b01f3a68fea3601771
Nigromante <nigromante@github.com>
2025-12-01 08:36:32 -0300

    rename libnum2str -> libnum2esstr

--- run.sh
+++ run.sh
@@ -2,6 +2,7 @@
 
 export COB_LIBRARY_PATH=~/Work/proyectos/cobol/BUILD/DEBUG/LIB
 
-make -f Makefile.test input=LIBNUM2STR module
-make -f Makefile.test input=NUM2STR compile
-make -f Makefile.test input=NUM2STR run
+make -f Makefile.test input=LIBNUM2ESSTR module
+make -f Makefile.test input=NUM2ESSTR compile
+make -f Makefile.test input=NUM2ESSTR run
+
