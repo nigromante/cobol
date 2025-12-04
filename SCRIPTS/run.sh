32ba860a2032a33b4df21bfc8ddbce820794dc29
Nigromante <nigromante@github.com>
2025-12-03 22:52:21 -0300

    split run script into   run and compile

--- SCRIPTS/run.sh
+++ SCRIPTS/run.sh
@@ -3,14 +3,5 @@ clear
 
 export COB_LIBRARY_PATH=~/Work/proyectos/cobol/BUILD/DEBUG/LIB
 
-# lib dependencies
-echo "-----------------------------"
-make -f ./SCRIPTS/Makefile.test input=LIBENNUM2STR module
-echo "-----------------------------"
-make -f ./SCRIPTS/Makefile.test input=LIBESNUM2STR module
-echo "-----------------------------"
-
-make -f ./SCRIPTS/Makefile.test input=NUM2STR compile
-echo "-----------------------------"
 make -f ./SCRIPTS/Makefile.test input=NUM2STR run
 
