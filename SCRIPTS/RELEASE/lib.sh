22c9ddd17fc82b69abaa4dc8ef5f6bb498a29b79
Nigromante <nigromante@github.com>
2025-12-22 23:23:27 -0300

    num2strxtra -> release

--- SCRIPTS/RELEASE/lib.sh
+++ SCRIPTS/RELEASE/lib.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+echo && make -f ./SCRIPTS/MAKEFILES/Makefile.release.module input=LIBNUM2STR
 echo && make -f ./SCRIPTS/MAKEFILES/Makefile.release.module input=LIBESNUM2STR
 echo && make -f ./SCRIPTS/MAKEFILES/Makefile.release.module input=LIBENNUM2STR
+echo && make -f ./SCRIPTS/MAKEFILES/Makefile.release.module input=LIBENTRY
 
 echo '----->'
 
