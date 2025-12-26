d69223edfe1f10771a5bec9b8855dd9145ce399f
Nigromante <nigromante@github.com>
2025-12-26 15:27:18 -0300

    strcopy full test

--- TEST/APPS/STR.cbl
+++ TEST/APPS/STR.cbl
@@ -9,14 +9,38 @@
        77  TEXTO1   PIC X(100).
        77  TEXTO2   PIC X(100).
        77  TEXTO3   PIC X(100).
+       77  XEOF     PIC XX.
 
        PROCEDURE DIVISION.
-
+           MOVE "_#" TO   XEOF.
+           MOVE "NOMBRE: _"        TO  TEXTO3.
            MOVE "JULIAN _ENrIQUE " TO TEXTO1.
-           MOVE "VIDAL_ALARCON  " TO TEXTO2.
-           STRING  TEXTO1 DELIMITED BY "_"
+           MOVE "VIDAL. _ALARCON  "  TO TEXTO2.
+
+           STRING  TEXTO3 DELIMITED BY "_"
+                   TEXTO1 DELIMITED BY "_"
                    TEXTO2 DELIMITED BY "_"
+                   XEOF  DELIMITED BY "#"
                    INTO TEXTO3.
+
+           STRING  TEXTO3 DELIMITED BY "_"
+                   TEXTO1 DELIMITED BY "_"
+                   TEXTO2 DELIMITED BY "_"
+                   XEOF  DELIMITED BY "#"
+                   INTO TEXTO3.
+
+           STRING  TEXTO3 DELIMITED BY "_"
+                   TEXTO1 DELIMITED BY "_"
+                   TEXTO2 DELIMITED BY "_"
+                   XEOF  DELIMITED BY "#"
+                   INTO TEXTO3.
+
+           STRING  TEXTO3 DELIMITED BY "_"
+                   TEXTO1 DELIMITED BY "_"
+                   TEXTO2 DELIMITED BY "_"
+                   XEOF  DELIMITED BY "#"
+                   INTO TEXTO3.
+
            DISPLAY TEXTO1.
            DISPLAY TEXTO2.
            DISPLAY TEXTO3.
