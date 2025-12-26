1194c1280634a7b22b805f8040a680227e3d3fa5
Nigromante <nigromante@github.com>
2025-12-26 16:25:05 -0300

    lib remove prevoius objects

--- SCRIPTS/TEST/lib.sh
+++ SCRIPTS/TEST/lib.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+rm -rf ./BUILD/DEBUG/LIB/*
 echo && make -f ./SCRIPTS/MAKEFILES/Makefile.test.module input=LIBNUM2STR
 echo && make -f ./SCRIPTS/MAKEFILES/Makefile.test.module input=LIBESNUM2STR
 echo && make -f ./SCRIPTS/MAKEFILES/Makefile.test.module input=LIBENNUM2STR
