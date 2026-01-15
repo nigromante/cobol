22e792c234fd52d845256d8c841e46c11e86740f
Nigromante <nigromante@github.com>
2026-01-15 19:49:08 -0300

    refactor and optimize english

--- TEST/CODE/NUM2ENSTR_CODE.cbl
+++ TEST/CODE/NUM2ENSTR_CODE.cbl
@@ -7,8 +7,6 @@
 
            PERFORM 300-CALCULATE.
 
-           MOVE  0   TO    W302-MILLAR.
-
            PERFORM 302-CDU-CALC THROUGH 302-SEGMENT
              VARYING W300-PART
              FROM 1 BY 1 UNTIL W300-PART > 4.
@@ -56,38 +54,33 @@
 
        302-CDU-CALC.
 
-           MOVE W300-TAB(W300-PART,1) TO W302-C.
-           MOVE W300-TAB(W300-PART,2) TO W302-D.
-           MOVE W300-TAB(W300-PART,3) TO W302-U.
-           MOVE W300-TAB(W300-PART,4) TO W302-CDU.
-           MOVE W300-TAB(W300-PART,5) TO W302-DU.
+           MOVE W300-TAB(W300-PART,1) TO W300-C.
+           MOVE W300-TAB(W300-PART,2) TO W300-D.
+           MOVE W300-TAB(W300-PART,3) TO W300-U.
+           MOVE W300-TAB(W300-PART,4) TO W300-CDU.
+           MOVE W300-TAB(W300-PART,5) TO W300-DU.
 
 
 
        302-SEGMENT.
 
-           PERFORM 302-CDU-CALC.
-
-           IF (W300-PART = 1 AND W302-CDU > 0)
+           IF (W300-PART = 1 AND W300-CDU > 0)
              PERFORM 302-CDU-PARSE
-               STRING  W300-RESULT DELIMITED BY "_"
-                 "BILLION _" DELIMITED BY "_"
-                 W300-EOF  DELIMITED BY "#"
-                 INTO W300-RESULT
+             STRING  W300-RESULT DELIMITED BY "_"
+               "BILLION _" DELIMITED BY "_"
+               W300-EOF  DELIMITED BY "#"
+             INTO W300-RESULT
            END-IF.
 
-           IF (W300-PART = 2)
-             COMPUTE W302-MILLAR = W302-CDU + W302-MILLAR
-             IF W302-MILLAR > 0
-               PERFORM 302-CDU-PARSE
-               STRING  W300-RESULT DELIMITED BY "_"
-                 "MILLION _" DELIMITED BY "_"
-                 W300-EOF  DELIMITED BY "#"
-                 INTO W300-RESULT
-             END-IF
+           IF (W300-PART = 2 AND W300-CDU > 0)
+             PERFORM 302-CDU-PARSE
+             STRING  W300-RESULT DELIMITED BY "_"
+               "MILLION _" DELIMITED BY "_"
+               W300-EOF  DELIMITED BY "#"
+             INTO W300-RESULT
            END-IF.
 
-           IF (W300-PART = 3 AND W302-CDU > 0)
+           IF (W300-PART = 3 AND W300-CDU > 0)
                PERFORM 302-CDU-PARSE
                STRING  W300-RESULT DELIMITED BY "_"
                  "THOUSAND _" DELIMITED BY "_"
@@ -95,7 +88,7 @@
                  INTO W300-RESULT
            END-IF.
 
-           IF ( W300-PART = 4 AND W302-CDU > 0 )
+           IF (W300-PART = 4 AND W300-CDU > 0)
              PERFORM 302-CDU-PARSE
            END-IF.
 
@@ -103,9 +96,9 @@
 
        302-CDU-PARSE.
 
-           IF W302-C > 0
+           IF W300-C > 0
              STRING  W300-RESULT DELIMITED BY "_"
-               W302-UNIDADES(W302-C) DELIMITED BY "_"
+               W302-UNIDADES(W300-C) DELIMITED BY "_"
                W300-EOF  DELIMITED BY "#"
                INTO W300-RESULT
 
@@ -115,26 +108,26 @@
                INTO W300-RESULT
            END-IF.
 
-           IF W302-D > 0
-             IF W302-D = 1 AND W302-U <> 0
+           IF W300-D > 0
+             IF W300-D = 1 AND W300-U <> 0
                STRING  W300-RESULT DELIMITED BY "_"
-                 W302-DECENAS-1(W302-U) DELIMITED BY "_"
+                 W302-DECENAS-1(W300-U) DELIMITED BY "_"
                  W300-EOF  DELIMITED BY "#"
-                 INTO W300-RESULT
-               MOVE 0 TO W302-U
+               INTO W300-RESULT
+               MOVE 0 TO W300-U
              ELSE
                STRING  W300-RESULT DELIMITED BY "_"
-                 W302-DECENAS(W302-D) DELIMITED BY "_"
+                 W302-DECENAS(W300-D) DELIMITED BY "_"
                  W300-EOF  DELIMITED BY "#"
                  INTO W300-RESULT
              END-IF
            END-IF.
 
-           IF W302-U > 0
-               STRING  W300-RESULT DELIMITED BY "_"
-                 W302-UNIDADES(W302-U) DELIMITED BY "_"
-                 W300-EOF  DELIMITED BY "#"
-                 INTO W300-RESULT
+           IF W300-U > 0
+             STRING  W300-RESULT DELIMITED BY "_"
+               W302-UNIDADES(W300-U) DELIMITED BY "_"
+               W300-EOF  DELIMITED BY "#"
+             INTO W300-RESULT
            END-IF.
 
 
--- TEST/CODE/NUM2ENSTR_DEF.cbl
+++ TEST/CODE/NUM2ENSTR_DEF.cbl
@@ -5,12 +5,3 @@
            77 W302-DECENAS-1   OCCURS 10 TIMES PIC X(20).
            77 W302-UNIDADES    OCCURS 10  TIMES PIC X(20).
 
-      *    Centenas/Decenas/Unidades
-           77 W302-CDU     PIC  999999 VALUE 0.
-           77 W302-DU      PIC  999999 VALUE 0.
-           77 W302-U  PIC 9.
-           77 W302-D  PIC 9.
-           77 W302-C  PIC 9.
-
-           77 W302-MILLAR  PIC  999999 VALUE 0.
-
