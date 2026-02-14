007cd597d448cd6ddf76fb954d9288d2b542a0fc
Nigromante <nigromante@github.com>
2026-02-14 15:42:41 -0300

    change runtime path

--- SCRIPTS/RELEASE/run.sh
+++ SCRIPTS/RELEASE/run.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-export COB_LIBRARY_PATH=~/Work/proyectos/cobol/BUILD/RELEASE/LIB
+export COB_LIBRARY_PATH=~/work/cobol/BUILD/RELEASE/LIB
 
 make -f ./SCRIPTS/MAKEFILES/Makefile.release input=$1 run
-
