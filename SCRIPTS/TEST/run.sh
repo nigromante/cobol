9de2d98fca8f7f545357ec676a26aed213d620e9
Nigromante <nigromante@github.com>
2026-02-21 21:25:45 -0300

    change test config

--- SCRIPTS/TEST/run.sh
+++ SCRIPTS/TEST/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export COB_LIBRARY_PATH=~/Work/proyectos/cobol/BUILD/DEBUG/LIB
+export COB_LIBRARY_PATH=~/work/cobol/BUILD/DEBUG/LIB
 
 make -f ./SCRIPTS/MAKEFILES/Makefile.test input=$1 run
 
